3651204bbf8efccb4716c2b666ebab225f6f76dd
Denys <123377581+Shtomuch@users.noreply.github.com>
2023-09-17 18:33:13 +0300

    Update config.js

--- site/.vitepress/config.js
+++ site/.vitepress/config.js
@@ -11,13 +11,15 @@ export default defineConfig({
   themeConfig: {
     nav: [{ text: "Лабораторні", link: "/labs/1" }],
 
-     sidebar: [
+    sidebar: [
       {
         text: "Лабораторні роботи",
         items: [
           { text: "Лабораторна робота №1", link: "/labs/1" },
           { text: "Лабораторна робота №2", link: "/labs/2" } // Додано нову сторінку
         ],
+      },
+    ],
 
     // socialLinks: [
     //   { icon: "github", link: "https://github.com/vuejs/vitepress" },
